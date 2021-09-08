4544d5baaa70c2e08e570a81b116e62bb8564b95
despenser08 <despenser08@gmail.com>
2021-09-08 12:11:42 -0500

    fix(serverinfo): Remove boost alert

--- src/commands/서버/serverinfo.ts
+++ src/commands/서버/serverinfo.ts
@@ -74,7 +74,7 @@ export default class extends Command {
           prompt: {
             optional: true,
             retry:
-              '"현재" | "투표" | "멤버" | "부스트" | "키워드"를 입력해 주세요.'
+              '"현재" | "투표" | "멤버" | "키워드"를 입력해 주세요.'
           },
           default: "now"
         },
