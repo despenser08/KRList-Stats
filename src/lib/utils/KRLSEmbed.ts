b20cedd0265fd267a571c2578d6698dba4b0cff0
despenser08 <despenser08@gmail.com>
2021-09-30 17:21:37 -0500

    fix(Embed): Check footer in data

--- src/lib/utils/KRLSEmbed.ts
+++ src/lib/utils/KRLSEmbed.ts
@@ -5,9 +5,21 @@ export default class KRLSEmbed extends MessageEmbed {
   constructor(data?: MessageEmbed | MessageEmbedOptions) {
     super(data);
 
-    if (!data) return this.setColor(Colors.PRIMARY);
-    this.setColor(data.color ?? Colors.PRIMARY);
+    if (!data) {
+      this.setColor(Colors.PRIMARY);
+      this.setFooter('이 봇은 "한국 디스코드 리스트"의 공식 봇이 아닙니다.');
+    } else {
+      this.setColor(data.color ?? Colors.PRIMARY);
 
-    this.setFooter('이 봇은 "한국 디스코드 리스트"의 공식 봇이 아닙니다.');
+      if (!data.footer)
+        this.setFooter('이 봇은 "한국 디스코드 리스트"의 공식 봇이 아닙니다.');
+      else
+        this.setFooter(
+          data.footer.text
+            ? `${data.footer.text} • 이 봇은 "한국 디스코드 리스트"의 공식 봇이 아닙니다.`
+            : '이 봇은 "한국 디스코드 리스트"의 공식 봇이 아닙니다.',
+          data.footer.iconURL
+        );
+    }
   }
 }
