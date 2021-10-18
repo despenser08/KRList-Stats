4f90d58b172cb8bd84a12fb2ada76f755adc1d4e
despenser08 <despenser08@gmail.com>
2021-10-18 10:07:56 +0900

    fix(createChart): Use backgroundColour

--- src/lib/utils/createChart.ts
+++ src/lib/utils/createChart.ts
@@ -41,7 +41,8 @@ export default function createChart(
     chartCallback: (chart) => {
       chart.defaults.font.family = "Noto Sans KR";
       chart.defaults.color = "#000";
-    }
+    },
+    backgroundColour: "white"
   });
   chart.registerFont(
     path.join(
@@ -57,19 +58,6 @@ export default function createChart(
       family: "Noto Sans KR"
     }
   );
-
-  if (!configuration.plugins) configuration.plugins = [];
-  configuration.plugins.unshift({
-    id: "white_background_color",
-    beforeDraw: (chart) => {
-      const ctx = chart.canvas.getContext("2d");
-      ctx.save();
-      ctx.globalCompositeOperation = "destination-over";
-      ctx.fillStyle = "white";
-      ctx.fillRect(0, 0, chart.width, chart.height);
-      ctx.restore();
-    }
-  });
 
   return chart.renderToBuffer(configuration, "image/png");
 }
