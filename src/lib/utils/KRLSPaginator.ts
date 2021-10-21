4d6859e1791af9531f4e2543c94b57b787cce14a
despenser08 <despenser08@gmail.com>
2021-10-22 06:36:11 +0900

    fix(KRLSPaginator): Fix import lint error

--- src/lib/utils/KRLSPaginator.ts
+++ src/lib/utils/KRLSPaginator.ts
@@ -15,7 +15,6 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { formatEmoji } from "@discordjs/builders";
 import { MessageActionRow, MessageButton } from "discord.js";
 import {
   ButtonPaginator,
