6f3be0f40112938ba44d481cd72d06546a244d95
despenser08 <despenser08@gmail.com>
2021-11-22 03:34:52 -0600

    fix(filterStats): Data range

--- src/lib/utils/filterStats.ts
+++ src/lib/utils/filterStats.ts
@@ -12,7 +12,7 @@ export default async function filterStats(
   let skip = 0;
 
   const quarters = await QuarterDB.find({}, {}, { sort: { start: -1 } });
-  let quarter = quarters[0];
+  let currentQuarter = quarters[0];
 
   if (limit instanceof Date) {
     const date = moment(limit).startOf("day");
@@ -20,14 +20,14 @@ export default async function filterStats(
     query.updated = { $gte: date.toDate(), $lte: nextDate.toDate() };
   } else if (typeof limit === "number" && Number.isInteger(limit) && statCount > limit) skip = statCount - limit;
   else if (limit === "quarter") {
-    if (quarters.length < 1 || !quarter) throw new Error("No quarters found");
+    if (quarters.length < 1 || !currentQuarter) throw new Error("No quarters found");
 
-    quarter = typeof endOfDateOrQuarter === "number" ? (await QuarterDB.findOne({ quarter: endOfDateOrQuarter })) ?? quarters[0] : quarters[0];
+    currentQuarter = typeof endOfDateOrQuarter === "number" ? (await QuarterDB.findOne({ quarter: endOfDateOrQuarter })) ?? quarters[0] : quarters[0];
 
-    const nextQuarter = await QuarterDB.findOne({ quarter: quarter.quarter + 1 });
-    if (nextQuarter) query.updated = { $gte: nextQuarter.start, $lt: quarter.start };
-    else query.updated = { $gte: quarter.start };
+    const nextQuarter = await QuarterDB.findOne({ quarter: currentQuarter.quarter + 1 });
+    if (nextQuarter) query.updated = { $gte: currentQuarter.start, $lt: nextQuarter.start };
+    else query.updated = { $gte: currentQuarter.start };
   }
 
-  return { query, skip, quarter: quarter.quarter };
+  return { query, skip, quarter: currentQuarter.quarter };
 }
